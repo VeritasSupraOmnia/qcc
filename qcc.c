76c87cad4ff7fc447bf10a7109a123ebfcabbeb3
VeritasSupraOmnia <keatonh99@gmail.com>
2021-07-24 11:53:10 -0700

    now with arg containment pars - haven't tested arg declarations

--- qcc.c
+++ qcc.c
@@ -1,6 +1,5 @@
-//#!/bin/tcc -run
-//Using tcc just for initial development loop
 //Quick C Compiler - A a psuedo-lisp to C transpiler.
+
 //buildflags {{{
 
 #define EMIT_SPACES false
@@ -49,7 +48,9 @@
 //passthrough to C mode
 #define prs_c_passth		0x100
 //asm conversion
-#define prc_c_asm			0x200
+#define prs_asm				0x200
+//tells the parser that an error has occurred.
+#define prs_fail			0x400
 
 #define pflgsz U2
 static pflgsz parse_flags=0;
@@ -63,17 +64,41 @@ static pflgsz parse_next_flags=0;
 int scope=0;
 //}}}
 
+//error flags{{{
+#define err_no_type_for_varlist 0x01
+
+#define eflgsz U2
+static eflgsz error_flags=0;
+//}}}
+
 
 //for testing
-char * qc_code=		"{#dmain[] 0 ret}";
-char * wanted_code=	"int main(){return 0;}";
+static char * wanted_code=	"int main(){return 0;}";
 
 //needed for full functioning
+static char * qc_code=		"{#dmain[] 0 ret}";
+int qc_size;
 char * result_code;
 int result_size=	0;
 
+static inline int maybeId(char * start){{{
+	//returns whether a current symbol might indicate an
+	//identifier
+	if(((	*start&0x5f	)<=	0x5a	&&	
+	   (	*start&0x5f	)>	0x40) 	||
+	   ((	*start&0x7f	)<=	0x7a	&&	
+	   (	*start&0x7f	)>	0x60))	return 1;
+	return 0;
+}}}
 
-int pushToOutput(char* symbol,int size){//{{{
+static inline int isWhitespace(char *start){{{
+	//just tells of the next character is whitespace
+	if(*start==' '||*start=='\n'||*start=='\t')
+		return 1;
+	return 0;
+}}}
+
+int pushToOutput(char* symbol,int size){{{
 	//increases the size of the output allocation while
 	//copying the same amount of bytes to the allocation
 	//basically just a copy but with the expansion of the
@@ -83,10 +108,9 @@ int pushToOutput(char* symbol,int size){//{{{
 	bcopy(symbol,&result_code[result_size],size);
 	result_size+=	size;
 	return 0;
-}//}}}
+}}}
 
-
-int addType(char * start){//{{{
+static inline int addType(char * start){{{
 
 	//Start is pointer at offset of the new type
 	//declaration. Directly adds the type, with a following
@@ -99,13 +123,12 @@ int addType(char * start){//{{{
 				break;
 		default: break;
 	}
-	parse_flags|=prs_new_id;
 	//parse_flags|=prs_was_op;
 	pushToOutput(temp,strlen(temp));
 	return i;//out;
-}//}}}
+}}}
 
-static inline int addId(char *start){//{{{
+static inline int addId(char *start){{{
 	//pushes an identifier to the output buffer which ends
 	//in whitespace or any other character that isn't
 	//aphabetical.
@@ -120,22 +143,114 @@ static inline int addId(char *start){//{{{
 
 	//remind the parser that it is no longer in "ID" state
 	//but was, just.
-	parse_flags|=prs_was_id;
-	parse_flags^=prs_new_id;
 	pushToOutput(start,i);
 	return i;
-}//}}}
+}}}
 
 
-static inline int toSymbol(char *start){//{{{
+static inline int toSymbol(char *start){{{
 	//Returns byte offset to when the next symbol is,
 	//ignoring spaces, tabs and newlines.
 	int i=0;char c=start[i];
 	while (c==' '||c=='\t'||c=='\n')
 			i++,c=start[i];
 	return i;//returns the amount to increase the parser increment counter.
-}//}}}
+}}}
 
+int printSyntaxErrorLocation(char *start,int local_loc){{{
+	//TODO: make this print the area around the current
+	//location, if there is room, so that people can
+	//understand where the syntax error is.
+	
+	//This OUGHT to be accompanied by a message describing
+	//the error, why it happened, what might fix it, as well
+	//as exactly where it is and in which file. This isn't
+	//related to the todo but I thought I should include it
+	//to remind myself.
+}}}
+
+static inline int addArgs(char * start){{{
+	//add argument declarations
+	int i=0;
+
+	//start the argument list with the needful
+	{char *temp="(";
+		pushToOutput(temp,1);}
+
+	//for doing comma based type repitition for multiple
+	//variable identifiers as well as error checking.
+	char* lastTypeLoc=(char*)0;
+	char* lastIdLoc=(char*)0;
+
+	U1 argstate=0;
+	#define argstate_inlist		0x01
+	#define argstate_done_ID	0x02
+
+	//do argument parsing
+	while (start[i]!=']'){
+		//clear whitespace
+		if (isWhitespace(start+i))
+				i+=toSymbol(start+i);
+
+		//check for varlists
+		if (start[i]==','){{{
+			//
+			//clear whatever whitespace there is
+			if (isWhitespace(start+i))
+				i+=toSymbol(start+i);
+
+
+			//Checking that a previous type and ID were
+			//declared within this var list and the comma
+			//isn't in the beginning of a new list.
+			if(	(argstate&argstate_inlist)	|| 
+				(argstate&argstate_done_ID))
+				//return the local location and set the
+				//parse fail flag for error reporting later
+				//on.
+				parse_flags|=prs_fail,
+				error_flags|=err_no_type_for_varlist;
+				return i;
+
+			//add comma to C output
+			pushToOutput(start+i,1);
+			
+			//add The last type to the output
+			addType(lastTypeLoc);
+				
+			//add the next ID in the varlist
+			i++,addId(start+i);
+
+
+		}}}else
+		//check for new type
+		if(start[i]=='#'){{{
+			lastTypeLoc=start+1;
+			i+=addType(start+1);
+			//clear argstate so that 
+			argstate^=argstate;
+			
+		}}}
+		//do id parsing
+		else if (maybeId(start+i)){
+			i+=addId(start+i);
+		}else break;
+
+		i++;
+	}
+
+	//end the argument
+	{char * temp=")";pushToOutput(temp,1);}
+
+	//reset the 
+	
+
+	return i;
+
+	//I don't need these defines elsewhere in the program
+	#undef argstate_inlist
+	#undef argstate_done_ID
+}}}
 
 int main(){
 	result_code=malloc(1);char * symbol=malloc(1);
@@ -145,36 +260,43 @@ start:
 
 		//For receiving symbol data for this iteration to
 		//add to the result_code string.
-		char temp=qc_code[i];
+		char *temp=&qc_code[i];
 
 		//for dealing with whitespace
-		if (temp==' '||temp=='\t')
-			i+=toSymbol(qc_code+i),
-			parse_flags|=prs_ws_sep_sym;//
+		if (isWhitespace(temp))
+			i+=toSymbol(temp),parse_flags|=prs_ws_sep_sym;
 
-		if (parse_flags & prs_new_id){
+		if (parse_flags & prs_new_id && maybeId(temp)){
 			//If a previous declaration says to expect a new
 			//identifier in the qc source
-			i+=addId(&qc_code[i]);
-		}else if(
+			i+=addId(temp);
+			parse_flags|=prs_was_id;
+			parse_flags^=prs_new_id;
+			if (isWhitespace(temp))
+				i+=toSymbol(temp),parse_flags|=prs_ws_sep_sym;
+		}/*else if(
 				(parse_flags & prs_was_id)		&&
 				(parse_flags & prs_new_scope))	{
+			
 
-		}else{//normal
-			switch(qc_code[i]){ 
-				case '#':
-							i+=addType(qc_code+i+1);
-							break; 
-				case '{': 
-							scope++;
-					 		parse_flags|=prs_new_scope;
-							break;
-				case '}':
-							char *brkt="}";
-							scope--;
-							break;
-
-			}
+		}*/
+		temp=&qc_code[i];
+		switch(*temp){ 
+			case '#':
+						i+=addType(temp+1);
+						parse_flags|=prs_new_id;
+						break; 
+			case '{': 
+						scope++,
+				 		parse_flags|=prs_new_scope;
+						break;
+			case '}':
+						scope--;char *brkt="}";
+						pushToOutput(brkt,1);
+						break;
+			case '[':
+						i+=addArgs(temp+1)+1;
+						break;
 		}
 	}
 	printf ("Wanted:\t%s\nResult:\t%s\n",wanted_code,result_code);
