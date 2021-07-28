201950abab6b4bd9922f20b054a36355453a399e
VeritasSupraOmnia <keatonh99@gmail.com>
2021-07-27 19:53:20 -0700

    updated test to test for newest features

--- tests/wanted.c
+++ tests/wanted.c
@@ -1,7 +1,10 @@
 int main(int argc,char **argv){
 	//this is a line comment
 	return 0;
+	break;
+	continue;
+	case '\"':
+	goto label;
 	/*this is a block comment that
    	keeps going after new lines*/
 }
-
